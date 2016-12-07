f56f3bbebd899ef0919a99f73d035133c945c95b
patrickcrocker <patrick.crocker@gmail.com>
2016-12-06 18:40:03 -0600

    added support for maven opts

--- scripts/build-maven.sh
+++ scripts/build-maven.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+#
+# All UPERCASE variables are provided externally from this script
 
 set -e
 
-VERSION=$(cat version/number)
+version=$(cat version/number)
+
+args="-DversionNumber=$version"
+[ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
+[ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
+[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
 
 pushd project
-  ./mvnw -PversionNumber=$VERSION clean package
+  ./mvnw clean package $args
 popd
 
-cp project/target/$ARTIFACT_ID-$VERSION.jar build-output/.
+cp project/target/$ARTIFACT_ID-$version.jar build-output/.
